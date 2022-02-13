149863d1aa58ce3e84116104a8553b1ef801c337
Kevin Johar <29691406+kevin-johar@users.noreply.github.com>
2022-02-13 06:56:39 -0500

    fix: app.listen moved to separate file to avoid reusing port 3000 when testing
    Signed-off-by: Kevin Johar <29691406+kevin-johar@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const express = require('express');
-const apiRouter = require('./routes/api');
-const app = express();
+import app from './server.js';
 
 const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use('/api', apiRouter);
-
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-})
-
-module.exports = app;
+});
