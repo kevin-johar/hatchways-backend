c134b3dfb240bd8e8643e74dd2c0bd66b62d782d
Kevin Johar <29691406+kevin-johar@users.noreply.github.com>
2022-02-13 06:41:51 -0500

    fix: both routes in working state
    Signed-off-by: Kevin Johar <29691406+kevin-johar@users.noreply.github.com>

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', apiRouter);
 
--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,12 @@ let router = express.Router();
 
 //GET Ping API
 router.get('/ping', (req, res) => {
-  res.status(200);
+  res.send({success: true}, 200);
 });
 
 //GET Posts filtered by tags,
 router.get('/posts', (req, res) => {
-  res.status(200);
+  res.send({success: true}, 200);
 });
 
 module.exports = router;
