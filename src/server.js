950c5575c0f7b7e99f0e92968d97e84fc78608f1
Kevin Johar <29691406+kevin-johar@users.noreply.github.com>
2022-02-13 05:30:47 -0500

    build: remove body parser
    Signed-off-by: Kevin Johar <29691406+kevin-johar@users.noreply.github.com>

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-
-export default app;
