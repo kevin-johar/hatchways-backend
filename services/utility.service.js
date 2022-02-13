2f1d364ac4bfaa2f6d86af33839cdff831fb48dc
Kevin Johar <29691406+kevin-johar@users.noreply.github.com>
2022-02-13 15:20:59 -0500

    feat: split up removing duplicates and sorting into two speparate functions
    Signed-off-by: Kevin Johar <29691406+kevin-johar@users.noreply.github.com>

--- services/utility.service.js
+++ services/utility.service.js
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { directionValues } from '../models/posts-request/directionValues.model.js';
 
 export const UtilityService = {};
 
@@ -8,8 +9,31 @@ UtilityService.convertTagsToArray = (tags) => {
   return strTags.split(',').map((tag) => tag.trim());
 };
 
-// Sorts and removes duplicates from an array
-UtilityService.sortAndFilterArray = (arr, sortBy, direction) => {
+UtilityService.removeDuplicates = (arr) => {
+  let noDuplicates = new Set();
+  for(let i = 0; i < arr.length; i++) {
+    noDuplicates[`${arr[i].id}`] = arr[i];
+
+  }
+  let uniquePosts = [];
+  for(const property in noDuplicates) {
+    uniquePosts.push(noDuplicates[property]);
+
+  }
+
+  return uniquePosts;
+}
+
+UtilityService.sortPosts = (arr, sortBy, direction) => {
+  // Sorts in place
+  return arr.sort(
+    (postA, postB) => {
+      console.log({direction, sortBy, postA: postA[sortBy], postB: postB[sortBy]});
+
+      return direction === directionValues.desc ?
+        postA[sortBy] - postB[sortBy] : // Descending
+      -1*(postA[sortBy] - postB[sortBy]); // Ascending
+    });
 }
 
 UtilityService.cachePosts = (posts, tag) => {
