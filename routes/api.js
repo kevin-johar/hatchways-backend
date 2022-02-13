d3016ca2f22b3436ada15bacfbe67a3d813d37f3
Kevin Johar <29691406+kevin-johar@users.noreply.github.com>
2022-02-13 08:43:51 -0500

    feat: /GET posts is returning collection of arrays based on tag
    Signed-off-by: Kevin Johar <29691406+kevin-johar@users.noreply.github.com>

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import { UtilityService } from '../services/utility.service.js';
+import { sortByValues } from '../models/posts-request/sortByValues.js';
+import { directionValues } from '../models/posts-request/directionValues.js';
+import { HttpService } from '../services/http.service.js';
 let router = express.Router();
 
 //GET Ping API
@@ -7,8 +11,26 @@ router.get('/ping', (req, res) => {
 });
 
 //GET Posts filtered by tags,
-router.get('/posts', (req, res) => {
-  res.status(200).send({success: true});
+router.get('/posts', async (req, res) => {
+  let error = {};
+  let hatchwayAPI = "https://api.hatchways.io/assessment/blog/posts";
+
+  if (!req.query || !req.query.tags) {
+    error.tags = "Tags parameter is required";
+  }
+  if (req.query.sortBy && (sortByValues.findIndex((value) => req.query.sortBy === value) === -1)) {
+    error.sortBy = "sortBy parameter is invalid";
+  }
+  if (req.query.direction && (directionValues.findIndex((value) => req.query.direction === value) === -1)) {
+    error.direction = "direction parameter is invalid";
+  }
+  if(Object.keys(error).length > 0) {
+    res.status(400).send({error});
+  }
+
+  const tags = UtilityService.convertTagsToArray(req.query.tags);
+  const posts = await Promise.all(tags.map((tag) => HttpService.getPostsByTag(hatchwayAPI + `?tag=${tag}`)));
+  res.send({posts}, 200);
 });
 
 export default router;
