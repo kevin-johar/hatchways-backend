9c321808e89db806b90d42cad19b6de9dbe50178
Atulpreet (Kevin) Johar <ajohar5@uwo.ca>
2022-02-15 02:34:06 -0500

    refactor: http request k

--- __tests__/api-ping.spec.js
+++ __tests__/api-ping.spec.js
@@ -4,19 +4,19 @@ import app from '../app.js';
 let request = supertest(app);
 const pingPath = '/api/ping';
 
-describe('GET /api/ping', () => {
+describe(`GET ${pingPath}`, () => {
   it('should return a 200 status code', async() => {
-    await request.get(pingPath, () => {
-    }).expect(200);
+    await request.get(pingPath)
+      .expect(200);
   });
 
   it('should specify json in the content header type', async() => {
-    await request.get(pingPath, () => {
-    }).expect("Content-Type", /json/);
+    await request.get(pingPath)
+      .expect("Content-Type", /json/);
   });
 
   it('should return body with success field equal to true', async() => {
-    await request.get(pingPath, () => {
-    }).expect({success: true});
+    await request.get(pingPath)
+      .expect({success: true});
   });
 });
