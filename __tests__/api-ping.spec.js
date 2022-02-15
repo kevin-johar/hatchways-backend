9d889da8a57528988e2bf8499aec701dffe31020
Atulpreet (Kevin) Johar <ajohar5@uwo.ca>
2022-02-14 22:01:39 -0500

    test: ping endpoint test complete

--- __tests__/api-ping.spec.js
+++ __tests__/api-ping.spec.js
@@ -1,16 +1,22 @@
 import supertest from 'supertest';
 import app from '../app.js';
 
+let request = supertest(app);
+const pingPath = '/api/ping';
+
 describe('GET /api/ping', () => {
   it('should return a 200 status code', async() => {
-    // Should return a status code of 200
-
-    await supertest(app).get('/api/ping', (res) => {
-      console.log(res);
+    await request.get(pingPath, () => {
     }).expect(200);
   });
 
-  it('should specify json in the content header type', () => {
+  it('should specify json in the content header type', async() => {
+    await request.get(pingPath, () => {
+    }).expect("Content-Type", /json/);
+  });
 
+  it('should return body with success field equal to true', async() => {
+    await request.get(pingPath, () => {
+    }).expect({success: true});
   });
 });
