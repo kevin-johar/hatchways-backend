8fef66c5952cdc31220d182556b05c6c39ac9063
Atulpreet (Kevin) Johar <ajohar5@uwo.ca>
2022-02-15 16:28:14 -0500

    test: utilities functions unit tests complete

--- __tests__/utilities.spec.js
+++ __tests__/utilities.spec.js
@@ -1,9 +1,7 @@
 import { UtilityService } from '../services/utility.service.js';
-import * as matchers from 'jest-extended';
 import path from 'path';
 import fs from 'fs';
 
-expect.extend(matchers);
 
 describe('[Utility Service] ', () => {
   describe('convertStringToArray', () => {
@@ -15,7 +13,6 @@ describe('[Utility Service] ', () => {
     ];
     test.each(stringSplitData)('%#. Given: %j Delimiter: %s Expected: %j', (given, delimiter, expected) => {
       const res  =UtilityService.convertStringToArray(given, delimiter);
-      expect(res).toBeArray();
       expect(res).toStrictEqual(expected);
     });
   });
@@ -29,6 +26,18 @@ describe('[Utility Service] ', () => {
     test.each(duplicateTestData())('should remove duplicate objects from array', (given, expected) => {
       const res = UtilityService.removeDuplicates(given);
       console.log(res);
+      expect(res).toStrictEqual(expected);
+    });
+  });
+
+  describe('sortPosts', () => {
+    const sortPostsTestData = () => {
+      const data = fs.readFileSync(path.resolve('__tests__/test-data/utilities.sort-posts.json'));
+      return JSON.parse(data.toString());
+    }
+
+    test.each(sortPostsTestData())('%#. %s', (testName, sortBy, direction, given, expected) => {
+      const res = UtilityService.sortPosts(given, sortBy, direction);
       expect(res).toStrictEqual(expected);
     });
   });
